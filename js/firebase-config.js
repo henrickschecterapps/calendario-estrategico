a708d6de5d361c5894da21028f4d9d76921d2fab
henrickschecterapps <henrickschecter@gmail.com>
2026-04-15 20:54:35 -0300

    Add files via upload

---
+++ js/firebase-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCmuDrq-ctHOGu4Fe9gHoK2Fxhcfg3BE9g",
+  authDomain: "calendario-estrategico.firebaseapp.com",
+  projectId: "calendario-estrategico",
+  storageBucket: "calendario-estrategico.firebasestorage.app",
+  messagingSenderId: "941508404506",
+  appId: "1:941508404506:web:af82455df11b35359538a3"
+};
